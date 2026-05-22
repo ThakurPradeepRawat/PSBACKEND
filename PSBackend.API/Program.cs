469f136e79111d3f3e07d801540ac04309a1e01e
Thakur Pradeep rawat <P.s.rawat4458@gmail.com>
2026-05-22 10:37:15 +0530

    update

--- PSBackend.API/Program.cs
+++ PSBackend.API/Program.cs
@@ -78,15 +78,14 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
   app.UseSwagger();
   app.UseSwaggerUI(options =>
   {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "PSBackend API V1");
     options.RoutePrefix = string.Empty; // Opens Swagger at root URL
   });
-}
+
 
 app.UseHttpsRedirection();
 
